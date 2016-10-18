a5d378a9d517287778293f199117f166f6849761
Josh Klodnicki <joshklod@gmail.com>
2016-10-18 13:27:29 -0400

    giti.bash: Automatically reads valid Git commands

--- giti.bash
+++ giti.bash
@@ -40,8 +40,15 @@ prompt() {
 	printf "$printf_str" "${PWD/#$HOME/\~}" "$head"
 }
 
+# Generate list of Git commands
+commands="$(
+	find /usr/libexec/git-core -type f -name git-* -perm /111 -print0 |
+	sed -z -e "s:.*/git-::" -e "s/\.exe$//" |
+	tr '\0' '|'
+)"
+
 # Set prompt:
-# GIT wdir (HEAD)
+# GIT wdir (HEAD -> ...)
 # >
 if [ $COLORS -ge 8 ]; then
 	printf_str="\n${blue}GIT $cyan%s $yellow(%b$yellow)\n$cyan> $reset"
@@ -54,20 +61,28 @@ fi
 
 # Main command loop
 while [ ! $exit_flag ]; do
+	# Regenerate every loop in case aliases change
+	aliases="$(
+		git config -l |
+		sed -e '/^[\t ]*alias/!d' -e 's/alias.//' -e 's/=.*//' |
+		tr '\n' '|'
+	)"
+	commandlist="$commands$aliases"
+	commandlist="(${commandlist%|})"
+	
 	prompt
 	
 	read -er
 	case "$REPLY" in
 		"")   continue ;;
 		exit) exit_flag=1 ;;
-		!*)
-			eval "${REPLY:1}"
-			;;
+		!*) eval "${REPLY:1}" ;;
 		*)
-			if [[ "$REPLY" =~ ^[[:blank:]]*(cd|ls|git)([[:blank:]].*)?$ ]]; then
-				eval "$REPLY"
-			else
+			if [[ "$REPLY" =~ ^[[:blank:]]*$commandlist([[:blank:]].*)?$ ]];
+			then
 				git $REPLY
+			else
+				eval "$REPLY"
 			fi
 	esac
 done
