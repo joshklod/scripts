cdb1c257795f4665063efc41f49a0a5a7ddb8afd
Josh Klodnicki <joshklod@gmail.com>
2017-06-12 14:59:17 -0400

    log.bash: Adds help info

--- log.bash
+++ log.bash
@@ -2,14 +2,31 @@
 #
 # log.bash: Run command with output redirected to logfile
 
+version='0.9'
+
 # Default mode
 mode="stdout"
 
 # Default directory to contain logfile
 [ -z "$LOGDIR" ] && LOGDIR="$HOME/.logs"
-
 # Create directory if it does not exist, or default to $HOME
 [ ! -d "$LOGDIR" ] && { mkdir "$LOGDIR" || LOGDIR="$HOME"; }
+
+function show-help {
+	echo -n "
+log.bash v$version
+Run command with output redirected to logfile
+
+  Usage: log [options] <command> [args]
+
+Options:
+  -o | --stdout   Redirect stdout to logfile
+  -e | --stderr   Redirect stderr to logfile
+  -b | --both     Redirect both stdout and stderr to logfile
+  -h | --help     Show this help info
+"
+	exit 0
+}
 
 # Process commandline args
 while [[ $1 == -* ]]; do
@@ -17,8 +34,9 @@ while [[ $1 == -* ]]; do
 		-o | --stdout ) mode="stdout" ;;
 		-e | --stderr ) mode="stderr" ;;
 		-b | --both   ) mode="both"   ;;
+		-h | --help   ) show-help     ;;
 		* )
-			echo "Unknown option '$1'" >&2
+			echo "Unknown option '$1' (Try 'log --help')" >&2
 			exit 1
 			;;
 	esac
